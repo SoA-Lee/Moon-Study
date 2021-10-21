8b5193d90b703fb649ff9d07bcba59148eeb29cd
dalgorithm <67897318+SoA-Lee@users.noreply.github.com>
2021-10-21 19:50:35 +0900

    [del] before

--- Machine_Learning&Deep_Learning/Tensorflow-NLP/textclassfication.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-import seaborn as sns
-# %matplotlib inline # 그래프를 주피터 노트북에서 바로 그리게 함
-
-train_data = pd.read_csv("word2vec-nlp-tutorial/labeledTrainData.tsv",header=0,delimiter="\t",quoting=3)
-train_data.head()
